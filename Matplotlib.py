6d5df6769ab440f61e16c57bdb94b45c43dea419
Shaik Jaffar <107207838+skjaffar@users.noreply.github.com>
2022-07-28 01:22:31 -0700

    Add files via upload

---
+++ Matplotlib.py
@@ -0,0 +1,51 @@
+# import matplotlib.pyplot as plt
+# x=[18,35,24,65,69,48]
+# rt=[0,1,6,9,7,30]
+# plt.bar(x,rt,color='red')
+# plt.show()    
+
+# import numpy as np
+# a = np.array(42)
+# b = np.array([1, 2, 3, 5])
+# c = np.array([[1, 2, 3], [4, 5, 6]])
+# d = np.array([[[1, 2, 3], [4, 5, 6]],[[1, 2, 3], [4, 5, 6]]])
+
+# print(a.ndim)
+# print(b.ndim)
+# print(c.ndim)
+# print(d.ndim)
+
+# import matplotlib.pyplot as plt 
+# import numpy as np
+# x = np.array([0, 6])
+# y = np.array([5, 10])
+# plt.plot(x,y)
+# plt.show()
+
+# import matplotlib.pyplot as plt
+# import numpy as np 
+# x = np.array([0, 6])
+# y = np.array([5, 10])
+# plt.plot(x, y, 'o')
+# plt.plot(x, y,'s')
+# plt.plot(x, y,'p')
+# plt.plot(x, y,'h')
+# plt.plot(x,y)
+# plt.show()
+
+# import matplotlib.pyplot as plt
+# import numpy as np 
+# x = np.array([1, 2, 6, 8])
+# y = np.array([3, 8, 1, 10])
+# plt.plot(x,y)
+# plt.plot(x, y,'p')
+# plt.plot(x, y,marker='p')
+# plt.plot(x, y,'s:k')
+# plt.plot(x, y,'s:c')
+# plt.plot(x, y,'s:y')
+# plt.plot(x, y,'s:w')
+# plt.plot(x, y,'s-b')
+# plt.plot(x, y,'s:r')
+# plt.plot(x, y,'s--g')
+# plt.show()
+
